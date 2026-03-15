bf9092b7bc8da1f35c2831a4ac08cf8f511764fd
Lisette Linse <lisette.linse@gmail.com>
2026-03-15 16:50:36 +0100

    Turn off the lights in common areas in the night

--- lib/scenes/building/components/room.dart
+++ lib/scenes/building/components/room.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
+import 'package:elevate/game.dart';
 import 'package:elevate/models/state/building_state.dart';
 import 'package:elevate/models/room_defs.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Room extends SpriteComponent {
+class Room extends SpriteComponent with HasGameReference<MyGame> {
   int floor;
   int roomIndex;
   RoomData roomData;
@@ -28,10 +29,16 @@ class Room extends SpriteComponent {
 
   @override
   void update(double dt) {
+    final lightsOn = switch (roomData.roomDef.roomType) {
+      RoomType.empty => true,
+      RoomType.office => roomData.nPeopleInRoom > 0,
+      RoomType.groundFloor => game.gameState.timeState.timeOfDay > 5 * 3600,
+      RoomType.garage =>
+        game.gameState.timeState.timeOfDay > 6 * 3600 &&
+            game.gameState.timeState.timeOfDay < 23 * 3600,
+    };
     _overlay.setColor(
-      roomData.roomDef.roomType != RoomType.office || roomData.nPeopleInRoom > 0
-          ? Colors.transparent
-          : Color.fromARGB(150, 0, 0, 0),
+      lightsOn ? Colors.transparent : Color.fromARGB(150, 0, 0, 0),
     );
     super.update(dt);
   }

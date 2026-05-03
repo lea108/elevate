99a26340ff399de5b4ba24c5b0eba6efd5cdf572
Lisette Linse <lisette.linse@gmail.com>
2026-05-03 12:29:11 +0200

    change: update org to se.tspquiz.elevate

---
+++ android/app/src/main/kotlin/se/tspquiz/elevate/elevate/MainActivity.kt
@@ -0,0 +1,5 @@
+package se.tspquiz.elevate.elevate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import gamepads_darwin
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  GamepadsDarwinPlugin.register(with: registry.registrar(forPlugin: "GamepadsDarwinPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}

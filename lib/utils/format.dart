7d3f1a9bbc3dc583489339ab80cd2f8c29c992d2
Lisette Linse <lisette.linse@gmail.com>
2026-08-15 18:50:34 +0200

    codechange: use final

--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -1,10 +1,9 @@
-
 String formatTimeOfDay(double time) {
-  int t = time.floor();
+  final int t = time.floor();
 
-  int h = (t/3600).floor();
-  int m = ((t - h * 3600) /60).floor();
-  int s = t - h * 3600 - m *60;
+  final int h = (t / 3600).floor();
+  final int m = ((t - h * 3600) / 60).floor();
+  final int s = t - h * 3600 - m * 60;
 
   //return '${h.toString().padLeft(2, '0')}:${m.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
   return '${h.toString().padLeft(2, '0')}:${m.toString().padLeft(2, '0')}';

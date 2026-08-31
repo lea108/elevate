3821f654e1372f9a06e4c79aea0f7d2d8900d5e4
Lisette Linse <lisette.linse@gmail.com>
2026-08-31 23:38:10 +0200

    fix: missing kIsWeb check

--- lib/utils/platform_adaptive.dart
+++ lib/utils/platform_adaptive.dart
@@ -1,9 +1,10 @@
-
 import 'dart:io';
 
 import 'package:elevate/utils/window/window.dart';
 import 'package:flutter/foundation.dart';
 
 bool get isDesktop {
-  return Platform.isMacOS || Platform.isWindows || Platform.isLinux || (kIsWeb && !isWebAndroid&& !isWebIOS);
+  return kIsWeb
+      ? !isWebAndroid && !isWebIOS
+      : Platform.isMacOS || Platform.isWindows || Platform.isLinux;
 }
